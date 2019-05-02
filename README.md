4147a54f0abbf8d7d9abf5a620b8f028416903e7
Abdusamed Sherif <abdusameds@gmail.com>
2019-05-02 12:53:54 -0700

    Weather App Functionality Complete

---
+++ README.md
@@ -0,0 +1,3 @@
+# Weather App
+
+This application is connected to a Weather API and gives the user the temperature, humidity, and weather conditions of his or her chosen location (City, Country).
---
+++ src/App.js
@@ -0,0 +1,89 @@
+import React from "react";
+
+import Titles from "./components/Titles"
+import Form from "./components/Form"
+import Weather from "./components/Weather"
+
+
+
+const API_KEY = "bfbd2238d7815817f1c22ecd473cb2ec";
+
+
+class App extends React.Component{
+
+  state = {
+
+    temperature: undefined,
+
+    city: undefined,
+
+    country: undefined,
+
+    humidity: undefined,
+
+    description: undefined,
+
+    error: undefined
+
+  }
+
+  getWeather = async (e) => {
+
+    e.preventDefault();
+
+    const city = e.target.elements.city.value;
+    const country = e.target.elements.country.value;
+
+    const API_CALL = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&APPID=${API_KEY}`);
+    const data = await API_CALL.json();
+    if (city && country) {
+      this.setState({
+        temperature: data.main.temp,
+        city: data.name,
+        country: data.sys.country,
+        humidity: data.main.humidity,
+        description: data.weather[0].description[0].toUpperCase() + data.weather[0].description.slice(1),
+        error: ""
+      });
+    } else {
+      this.setState({
+        temperature: undefined,
+        city: undefined,
+        country: undefined,
+        humidity: undefined,
+        description: undefined,
+        error: "Please enter the values."
+      });
+    }
+  }
+  render() {
+    return (
+      <div>
+        <div className="wrapper">
+          <div className="main">
+            <div className="container">
+              <div className="row">
+                <div className="col-xs-5 title-container">
+                  <Titles />
+                </div>
+                <div className="col-xs-7 form-container">
+                  <Form getWeather={this.getWeather} />
+                  <Weather 
+                    temperature={this.state.temperature} 
+                    humidity={this.state.humidity}
+                    city={this.state.city}
+                    country={this.state.country}
+                    description={this.state.description}
+                    error={this.state.error}
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+};
+
+export default App;
